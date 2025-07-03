24e874f21ffc14678d49819cf8f84ee980759571
bwbohl <b.w.bohl@gmail.com>
2025-07-03 22:49:39 +0200

    Create MeiOxygenPlugin.java

---
+++ src/main/java/org/music_encoding/MeiOxygenPlugin.java
@@ -0,0 +1,11 @@
+package org.music_encoding;
+
+/**
+ * Hello MEI!
+ */
+
+class MeiOxygenPlugin {
+    public static void main(String[] args) {
+        System.out.println("Hello, MEI!"); 
+    }
+}

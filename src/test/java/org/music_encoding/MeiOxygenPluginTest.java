777f5229c42fca49cb3d0c48e81f09b295b3fef7
bwbohl <b.w.bohl@gmail.com>
2025-07-03 22:51:51 +0200

    Create MeiOxygenPluginTest.java

---
+++ src/test/java/org/music_encoding/MeiOxygenPluginTest.java
@@ -0,0 +1,19 @@
+package org.music_encoding;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for MeiOxygenPlugin.
+ */
+public class MeiOxygenPluginTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}

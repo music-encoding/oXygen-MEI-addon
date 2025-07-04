afbb8ea26066ff7b2bf76f3ac7d3fe346eaf09b5
bwbohl <b.w.bohl@gmail.com>
2025-07-04 10:33:00 +0200

    Update MeiOxygenPlugin.java

--- src/main/java/org/music_encoding/MeiOxygenPlugin.java
+++ src/main/java/org/music_encoding/MeiOxygenPlugin.java
@@ -1,11 +1,39 @@
-package org.music_encoding;
+package org.music_encoding.oxygen.plugin;
+
+import ro.sync.exml.plugin.Plugin;
+import ro.sync.exml.plugin.PluginDescriptor;
 
 /**
  * Hello MEI!
  */
 
-class MeiOxygenPlugin {
-    public static void main(String[] args) {
-        System.out.println("Hello, MEI!"); 
+public class MeiOxygenPlugin extends Plugin {
+  /**
+   * The static plugin instance.
+   */
+    private static MeiOxygenPlugin instance = null;
+
+    /**
+     * Construct the plugin.
+     *
+     * @param descriptor The plugin descriptor
+     */
+    public MeiOxygenPlugin(PluginDescriptor descriptor) {
+
+      super(descriptor);
+
+      if (instance != null) {
+        throw new IllegalStateException("Already instantiated!");
+      }
+      instance = this;
     }
+
+  /**
+   * Get the plugin instance.
+   *
+   * @return the shared plugin instance.
+   */
+  public static MeiOxygenPlugin getInstance() {
+    return instance;
+  }
 }
